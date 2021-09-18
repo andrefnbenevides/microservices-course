a3ba1bf047c0cfe1e7c7d4b670bc263ec7b7aab2
andrefnbenevides <andre.rhagnar93@gmail.com>
2021-09-18 17:25:09 +0100

    feat: add send sync data classes

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,38 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using PlatformService.Dtos;
+using System.Text;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto platform)
+        {
+            var httpContent = new StringContent(JsonSerializer.Serialize(platform), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync POST to Commands Service was OK");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync POST to Commands Service was NOT OK");
+            }
+        }
+    }
+}
